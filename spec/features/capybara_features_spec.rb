e302ca7f1186fb6146d9559c1bb233546a82939f
Iryna Howarth <irynahowarth@gmail.com>
2016-02-03 18:40:22 +0000

    Viewing hit points Player2

--- spec/features/capybara_features_spec.rb
+++ spec/features/capybara_features_spec.rb
@@ -9,4 +9,15 @@ feature "User Story => Battle" do
     click_button('Submit')
     expect(page).to have_content("Iryna vs. Barry")
   end
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  scenario "allow to see player's 2 Hit Pounts" do
+    visit '/'
+    fill_in('player1_name', with: 'Iryna')
+    fill_in('player2_name', with: 'Barry')
+    click_button('Submit')
+    expect(page).to have_content("Barry: 60 HP")
+  end
 end
