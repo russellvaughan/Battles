86886be0c9069c7f565af0c390288a7bf6004d53
Iryna Howarth <irynahowarth@gmail.com>
2016-02-03 18:19:17 +0000

    POST/redirect /GET pattern

--- spec/features/capybara_features_spec.rb
+++ spec/features/capybara_features_spec.rb
@@ -4,8 +4,8 @@ feature "User Story => Battle" do
   # We want to Start a fight by entering our names and seeing them
   scenario "allow players entering their names" do
     visit '/'
-    fill_in('player1', with: 'Iryna')
-    fill_in('player2', with: 'Barry')
+    fill_in('player1_name', with: 'Iryna')
+    fill_in('player2_name', with: 'Barry')
     click_button('Submit')
     expect(page).to have_content("Iryna vs. Barry")
   end
